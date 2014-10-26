ca9aec0e35b39193bd26fa6ee80a04ce8617a3df
Ryan Zhou <ryanzhou7@gmail.com>
2014-10-26 01:43:03 -0700

    sutffffs

---
+++ src/itp341/exercises/week7_prefactivity/find.java
@@ -0,0 +1,12 @@
+package itp341.exercises.week7_prefactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class find extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.findlayout);
+	}
+	
+}
